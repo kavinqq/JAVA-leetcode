10a3f77b9809671df0bef76888e4448357555be9
KuanLin <36700601+kavinqq@users.noreply.github.com>
2022-01-13 00:15:56 +0800

    Update mergeTwoLists.java

--- mergeTwoLists.java
+++ mergeTwoLists.java
@@ -36,20 +36,12 @@ class Solution {
                 list2 = list2.next;
             }                
         }// end while   
-        while(list1 != null){
-            node = new ListNode(list1.val);
-            node.next = null;
-            list3.next = node;
-            list3 = list3.next;
-            list1 = list1.next;            
+        //因為題目有預設 兩個是已經排序好的鏈結串列 所以剩下的一定都 >= 目前節點 直接放就好
+        if(list1 != null){
+            list3.next = list1;
         }
-        while(list2 != null){
-            node = new ListNode(list2.val);
-            node.next = null;
-            list3.next = node;
-            list3 = list3.next;
-            list2 = list2.next;
-            
+        else{           
+            list3.next = list2;
         }
         list3 = head3.next;
         return list3;
