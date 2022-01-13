b86803459968cf97d3835cb770fa2094b92f70a4
KuanLin <36700601+kavinqq@users.noreply.github.com>
2022-01-14 00:30:03 +0800

    Update and rename Implement strStr().java to (超時版&改良版)Implement strStr().java

--- (超時版&改良版)Implement strStr().java
+++ (超時版&改良版)Implement strStr().java
@@ -1,7 +1,9 @@
 /*
 都對 但是
 Time Limit Exceeded!!!
-*/
+
+留著作警惕 可以看看自己在第一次寫 有多少多餘的敘述
+
 class Solution {
     public int strStr(String haystack, String needle) {                
         if(haystack.equals(null) || needle.equals(null) || needle.length() == 0){
@@ -43,5 +45,36 @@ class Solution {
             return ans;
         else
             return -1;
+    }
+}
+
+*/
+
+
+class Solution {
+    public int strStr(String haystack, String needle) {                
+        if(needle.length() == 0){
+            return 0;
+        }                
+        
+        for(int i = 0; i < haystack.length(); i ++){                   
+            int tmpIndex = i;
+            if(haystack.length() - i < needle.length()){
+                break;
+            }
+            for(int j = 0; j < needle.length(); j ++){
+                
+                if(haystack.charAt(tmpIndex) != needle.charAt(j) ){
+                    break;
+                }
+                else{
+                    tmpIndex += 1;
+                }
+                if(j == needle.length() - 1){
+                    return i;
+                }
+            }//end for() line 7
+        }//end for() line 12
+        return -1;
     }
 }
